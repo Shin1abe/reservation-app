9852e3fcf1ecc40be689151aeb77d0a87913fb79
Shin1 <shin1abewk@gmail.com>
2021-01-17 08:46:22 +0900

    Nodeサーバ構築

---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    DB_URI : 'mongodb+srv://testtest:scp8OoPo8aCcB8Dd@cluster0.3gnl8.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ server/routes/product.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const Product = require('../model/product')
+
+router.get('', function(req, res){
+    Product.find({}, function(err, foundProducts){
+       return res.json(foundProducts)
+    })
+})
+
+router.get('/:product_id', function(req, res){
+    const productid = req.params.product_id
+    Product.findById(productid, function(err, foundProduct){
+        if(err){
+            return res.status(422).send({
+                errors:[
+                    {
+                        title: 'Product Error',
+                        detail: 'Product not Found'
+                    }
+                ]
+            })
+        }
+        return res.json(foundProduct)
+    })
+})
+
+module.exports = router
