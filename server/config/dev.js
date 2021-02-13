17e0bbca6eddf355efa7a1c8fb3768a752c3859f
BettutiAd <shin1abewk@gmail.com>
2021-02-13 11:20:56 +0900

    AuthGuard実装

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,4 +1,5 @@
 module.exports = {
-    DB_URI : 'mongodb+srv://testtest:scp8OoPo8aCcB8Dd@cluster0.3gnl8.mongodb.net/test?retryWrites=true&w=majority',
+    // DB_URI : 'mongodb+srv://testtest:scp8OoPo8aCcB8Dd@cluster0.3gnl8.mongodb.net/test?retryWrites=true&w=majority',//開発 NG 2021/0221
+    DB_URI : 'mongodb+srv://dbReserve:testtest1@cluster0.qufol.mongodb.net/<dbname>?retryWrites=true&w=majority',//本番 2021/0221
     SECRET : 'fUjXn2r5u8x/A?D(G+KbPdSgVkYp3s6v'
 }
